7d6c94acd973e84fa9b8c81afbdb0bfac3d18466
xielinyao <xiely2000@sina.cn>
2020-11-22 15:29:32 +0800

    first verison

---
+++ src/main/java/com/example/pes/PesApplication.java
@@ -0,0 +1,13 @@
+package com.example.pes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesApplication.class, args);
+	}
+
+}
