e58d0376d5d077e4f4f948ac1c8aae53e7fbce36
xielinyao <xiely2000@sina.cn>
2020-11-22 15:27:13 +0800

    first version

---
+++ src/test/java/com/example/pes/PesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
